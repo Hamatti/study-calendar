bc1eaa5187727f10b8b2263f6677acbe8b28e84d
Juha-Matti Santala <juhamattisantala@gmail.com>
2013-10-14 12:48:01 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 study-calendar
 ==============
 
-Create Google Calendar compatible CSV of lectures
+Small script to ease out with the problem of Google Calendar not supporting
+recurring events with csv import.
+
+In the *opinnot_raw.csv* file I have starting and ending date for each lecture
+plus starting time of each lecture. The script then generates ending time (+2
+hrs) and creates event for each lecture between start and end date (inclusive).
+
+*(All code comments are in Finnish)*
---
+++ to_cal.py
@@ -0,0 +1,50 @@
+#coding=utf-8
+#! /usr/bin/python
+
+# 2. periodin opintojen csv-tiedoston luominen Google-kalenteria varten
+# Juha-Matti Santala
+
+import datetime, time
+import csv
+
+# Google-kalenteri vaatii tietynlaisesti nimetyt otsikot
+HEADER = "Subject,Start Date,Start Time,End Date,End Time".split(',')
+
+
+CSVFILE = "opinnot.csv"
+INFILE = "opinnot_raw.csv"
+writer = csv.writer(open(CSVFILE, 'w'))
+infile_reader = csv.reader(open(INFILE).readlines()[1:])
+
+
+# Google-kalenteri tykkää tästä formaatista päivämäärille<
+DAYFORMAT = "%m/%d/%Y"
+
+# Luetaan sisään kurssi-informaatio
+courses = [course for course in infile_reader]
+
+# Kirjoitetaan otsikot
+writer.writerow(HEADER)
+
+# Laajennetaan kurssit kattamaan koko väli START_DATE - END_DATE
+for course in courses:
+    subject = course[0]
+    start_date = datetime.datetime.strptime(course[1], DAYFORMAT)
+    end_date = datetime.datetime.strptime(course[2], DAYFORMAT)
+    start_time = course[3]
+    # Luennon päättymisaika 2h luennon alkamisen jälkeen. Mikäli luento alkaa
+    # klo 10 jälkeen, on päättymisaika PM, muuten AM
+    if int(start_time[:2]) < 10:
+        end_time = "%d:00 AM" % (int(start_time[:2])+2)
+    else:
+        end_time = "%d:00 PM" % (int(start_time[:2]) + 2)
+
+    # Luodaan tapahtumia päättymispäivään asti
+    moving_date = start_date
+    while moving_date <= end_date:
+        print_date = moving_date.strftime(DAYFORMAT)
+        writer.writerow([subject, print_date, start_time, print_date, end_time])
+        # Luento toistuu viikon välein
+        moving_date = moving_date + datetime.timedelta(days=7)
+
+
